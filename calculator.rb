3ec75d1a21352a1e1652ec10932750f6b19e16cc
compiled-convention-1791 <melody.bebee@gmail.com>
2020-08-07 01:31:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,7 @@
 # Add your variables here
-first_number = 1.0
+first_number = 5.0
 second_number = 4.8
+
 sum = first_number+second_number
+difference = first_number-second_number
+product = first_number*second_number
